61646ada170e7352bf36673229e2fd794522bfc0
JunaidSarfraz <juneed.sarfraz@gmail.com>
2015-10-23 18:40:06 +0500

    devise integrated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,19 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  #Devise  Action mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "www.gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "official.core.factory@gmail.com",
+    password: "Corefactory123.."
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
