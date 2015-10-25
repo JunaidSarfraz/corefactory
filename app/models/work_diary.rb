b218db5ae299eb14e0a1250566733edfbefbddf2
JunaidSarfraz <juneed.sarfraz@gmail.com>
2015-10-25 22:14:11 +0500

    some fixes

--- app/models/work_diary.rb
+++ app/models/work_diary.rb
@@ -1,4 +1,4 @@
 class WorkDiary < ActiveRecord::Base
-	belongs_to: pay_head
-	belongs_to: emplyee, :class_name => "User", :foreign_key => "user_id"
+	belongs_to :pay_head
+	belongs_to :emplyee, :class_name => "User", :foreign_key => "user_id"
 end
