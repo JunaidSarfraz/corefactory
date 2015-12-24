0f215eebd8243f6c8101a7bdf7b292b39d7c0d5a
JunaidSarfraz <juneed.sarfraz@gmail.com>
2015-12-24 13:58:03 +0500

    Handle column already exist exception

--- db/migrate/20151129121029_add_more_info_in_users.rb
+++ db/migrate/20151129121029_add_more_info_in_users.rb
@@ -1,10 +1,10 @@
 class AddMoreInfoInUsers < ActiveRecord::Migration
   def change
-  	add_column	:users,	:fax,				:string
-  	add_column	:users,	:secondary_email,	:string
-  	add_column	:users,	:ntn,				:string
-  	add_column	:users,	:refferal_id,			:integer
-  	add_column	:users,	:social_media_links,	:text
-  	add_column	:users,	:home_contact,		:text
+  	add_column	:users,	:fax,				:string rescue puts "column already added"
+  	add_column	:users,	:secondary_email,	:string rescue puts "column already added"
+  	add_column	:users,	:ntn,				:string rescue puts "column already added"
+  	add_column	:users,	:refferal_id,			:integer rescue puts "column already added"
+  	add_column	:users,	:social_media_links,	:text rescue puts "column already added"
+  	add_column	:users,	:home_contact,		:text rescue puts "column already added"
   end
 end
