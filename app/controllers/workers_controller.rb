af3a33bb939cdd655f996aae745fc1c53c5f6a68
Junaid Sarfraz <juneed.sarfraz@gmail.com>
2016-07-11 07:00:29 +0500

    Add workers account tab and fix accounts tab code replication and make a single partial for workers, suppliers, clients and company account tab

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -51,9 +51,53 @@ class WorkersController < ApplicationController
 		@worker = User.find(params[:id].to_i)
 	end
 	def worker_params
-		params.require(:user).permit(:first_name, :middle_name, :last_name, :gender, :cnic, :cast,
-		:religion, :email, :password, :password_confirmation, :address, :city, :country, :zip_code,
-		:primary_phone, :secondary_phone, :disabled, :_type, :blood_group, :height, :join_date, 
-		:branch_id, :hobbies_attributes => [:id, :name,:description,:_destroy])
+		params.require(:user).permit(
+			:first_name, 
+			:middle_name, 
+			:last_name, 
+			:gender, 
+			:cnic, 
+			:cast,
+			:religion, 
+			:email, 
+			:password, 
+			:password_confirmation, 
+			:address, 
+			:city, 
+			:country, 
+			:zip_code,
+			:primary_phone, 
+			:secondary_phone, 
+			:disabled, 
+			:_type, 
+			:blood_group, 
+			:height, 
+			:join_date, 
+			:branch_id, 
+			:hobbies_attributes => [
+				:id, 
+				:name,
+				:description,
+				:_destroy
+			],
+			:accounts_attributes => [
+				:id,
+				:title,
+				:description,
+				:_type,
+				:bank_name,
+				:bank_account_number,
+				:bank_address,
+				:bank_city,
+				:bank_state,
+				:bank_country,
+				:bank_zip_code,
+				:bank_primary_phone,
+				:bank_secondary_phone,
+				:current_balance,
+				:account_holder_type,
+				:_destroy
+			]
+		)
 	end
 end
